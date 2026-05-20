e34bf2e581c7c2769d66bc8f81675891545bfd1a
smsixmasum-source <smsix.masum@gmail.com>
2026-05-20 04:54:57 +0300

    Add color definitions for theme

---
+++ app/src/main/java/com/tasbih/couple/presentation/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.tasbih.couple.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val IslamicGreen = Color(0xFF1B5E20)
+val IslamicGreenLight = Color(0xFF4CAF50)
+val IslamicGreenSurface = Color(0xFFE8F5E9)
+val GoldAccent = Color(0xFFD4AF37)
+val GoldLight = Color(0xFFFFF8E1)
+val CreamWhite = Color(0xFFFAFAF5)
+val DarkSurface = Color(0xFF0D1F0D)
+val DarkCard = Color(0xFF1A2E1A)
+val ErrorRed = Color(0xFFB00020)
